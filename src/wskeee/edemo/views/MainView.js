3439f58a5e0feb6e3f45ea2f422d150d91e55f9a
wskeee <361733529@qq.com>
2017-02-22 18:08:45 +0800

    1、测试export default class 2、建立MainView

--- src/wskeee/edemo/views/MainView.js
+++ src/wskeee/edemo/views/MainView.js
@@ -1,5 +1,17 @@
 /**
  * Created by wskeee on 2017-02-22 .
  */
+'use strict';
 import React,{Component} from 'react';
-import {Component} from 'react-native';
+import {Text} from 'react-native';
+
+export default class extends Component{
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return (
+            <Text>MainView</Text>
+        );
+    };
+}
